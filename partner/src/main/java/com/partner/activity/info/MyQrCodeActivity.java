74101c2246edf8de93882954548a5de069cba892
yuyangmin <yuyangmin@uboxol.com>
2015-06-30 23:10:55 +0800

    Merge branch 'master' of /home/emilyu/android/studioworkspace/Partner_V1.0.0 with conflicts.

--- partner/src/main/java/com/partner/activity/info/MyQrCodeActivity.java
+++ partner/src/main/java/com/partner/activity/info/MyQrCodeActivity.java
@@ -1,9 +1,13 @@
 package com.partner.activity.info;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.partner.PartnerApplication;
 import com.partner.R;
 import com.partner.activity.base.BaseActivity;
@@ -15,6 +19,12 @@ public class MyQrCodeActivity extends BaseActivity {
 
 	@ViewId(R.id.view_title)
 	private TitleView titleView;
+
+	@ViewId(R.id.im_avatar)
+	private SimpleDraweeView avatarView;
+
+	@ViewId(R.id.tv_name)
+	private TextView nameView;
 
 	private static final String TAG = MyQrCodeActivity.class.getSimpleName();
 
@@ -30,6 +40,12 @@ public class MyQrCodeActivity extends BaseActivity {
 	@Override
 	protected void initControls(Bundle savedInstanceState) {
 		titleView.setTitle(R.string.my_qrcode);
+		nameView.setText(PartnerApplication.getInstance().getUserInfo().getUserName());
+		String avatarImage = PartnerApplication.getInstance().getUserInfo().getHeadImage();
+		if(!TextUtils.isEmpty(avatarImage)) {
+			Uri uri = Uri.parse(avatarImage);
+			avatarView.setImageURI(uri);
+		}
 	}
 
 	@Override
